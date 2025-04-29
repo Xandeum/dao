88f0535e8e950cea05df87d356f62676eb591fa0
Bernie Blume <bernie.blume@antsle.com>
2025-04-28 18:29:44 -0700

    udpate endpoints

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,9 +1,9 @@
 export const MAINNET_RPC =
   process.env.NEXT_PUBLIC_MAINNET_RPC ||
-  process.env.MAINNET_RPC ||
-  'http://realms-realms-c335.mainnet.rpcpool.com'
+  process.env.MAINNET_RPC || 'https://mainnet.helius-rpc.com/?api-key=2aca1e9b-9f51-44a0-938b-89dc6c23e9b4'
+//  'http://realms-realms-c335.mainnet.rpcpool.com'
 
 export const DEVNET_RPC =
   process.env.NEXT_PUBLIC_DEVNET_RPC ||
-  process.env.DEVNET_RPC ||
-  'https://mango.devnet.rpcpool.com'
+  process.env.DEVNET_RPC || 'https://devnet.helius-rpc.com/?api-key=2aca1e9b-9f51-44a0-938b-89dc6c23e9b4'
+//  'https://mango.devnet.rpcpool.com'
